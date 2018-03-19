abd6824e9281aca64161cd3802bdb4090f11534a
Bolivar Pereira <bolivar.pereira@acad.pucrs.br>
2018-03-19 17:42:19 -0300

    Criação da classe Leitor

--- MarsRover/src/Leitor.java
+++ MarsRover/src/Leitor.java
@@ -1,8 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Leitor {
 
+	private BufferedReader leitor;
+
 	public Leitor() {
-		// TODO Auto-generated constructor stub
+		this.leitor = null;
+	}
+
+	public List<String> le(String arquivo) {
+		List<String> retorno = new ArrayList<String>();
+
+		try {
+
+           leitor = new BufferedReader(new FileReader(arquivo));		
+
+			String linha = leitor.readLine();
+
+			while (linha != null) {
+				retorno.add(linha);
+				linha = leitor.readLine();
+			}
+
+		}
+
+		catch(IOException e) { e.printStackTrace(); } 
+
+        finally { 
+			
+			try { if (leitor != null) leitor.close(); }
+			catch(IOException e) { e.printStackTrace(); }
+			
+		}
+
+		return retorno;
 	}
 
 }
